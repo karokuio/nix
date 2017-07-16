d559ac1be8117c228a6d12067b861ad406ea4d4a
Mario Garcia <mario.ggar@gmail.com>
2017-07-16 18:44:06 +0200

    add definitions of events to a class to avoid typos

--- src/main/groovy/nix/DockerEventsPublisher.groovy
+++ src/main/groovy/nix/DockerEventsPublisher.groovy
@@ -5,14 +5,25 @@ import pluto.events.Publisher
 import com.github.dockerjava.api.model.Event
 import com.github.dockerjava.core.command.EventsResultCallback
 
+/**
+ * Callback used by nix to send every Docker event to the message
+ * broker
+ *
+ * @since 0.1.0
+ */
 class DockerEventsPublisher extends EventsResultCallback {
 
+  /**
+   * Broker message publisher instance
+   *
+   * @since 0.1.0
+   */
   @Inject
   Publisher publisher
 
   @Override
   void onNext(Event event) {
-    publisher.publish('event.docker', event)
+    publisher.publish(Events.DOCKER, event)
     super.onNext(event);
   }
 }
---
+++ src/main/groovy/nix/Events.groovy
@@ -0,0 +1,16 @@
+package nix
+
+/**
+ * Here you can find all even type definitions
+ *
+ * @since 0.1.0
+ */
+class Events {
+
+  /**
+   * Represents a Docker event
+   *
+   * @since 0.1.0
+   */
+  static final String DOCKER = 'event.docker'
+}
